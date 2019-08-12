a30be64ec21bb68ebe95922ee68c1634b409d563
ematysek <ematysek@redhat.com>
2019-08-12 13:50:37 -0400

    Improve time complexity for missing nums

--- logging_metrics_performance/enterprise_logging/utils/logtest_verification/verify_logtest_index.py
+++ logging_metrics_performance/enterprise_logging/utils/logtest_verification/verify_logtest_index.py
@@ -179,30 +179,38 @@ def verify_els_messages(els_json: dict, max_expected):
     print("Expected max number: {}".format(max_expected))
 
     # Find number of occurrences for each message_num
-    duplicates_found = 0
+    # duplicates_found = 0
     # missing_found = 0
-    missing_nums = []
+    # missing_nums = []
     # print(max_expected)
+
+    num_tracker = {i: 0 for i in range(1, max_expected+1)}
+    for i in message_num_list:
+        num_tracker[i] += 1
+    missing_nums = [i for i in num_tracker if num_tracker[i] == 0]
+    # TODO handle duplicates
+    # duplicate_nums = [i for i in num_tracker if num_tracker[i] > 1]
+    duplicates_found = len([i for i in num_tracker if num_tracker[i] > 1])
     # Currently assume all message ranges should go from 1-
-    for i in range(1, max_expected + 1):
+    # for i in range(1, max_expected + 1):
         # if i > max_num:
         #     print("Missing: {} - {}".format(i, max_expected))
         #     missing_found += (max_expected - i)
         #     break
-        count = message_num_list.count(i)
-        if count > 1:
-            print("Duplicate: {} - Count: {}".format(i, count))
-            duplicates_found += 1
-        elif count == 0:
+        # count = message_num_list.count(i)
+        # if count > 1:
+        #     print("Duplicate: {} - Count: {}".format(i, count))
+        #     duplicates_found += 1
+        # elif count == 0:
             # print("Missing: {}".format(i))
-            missing_nums.append(i)
+            # missing_nums.append(i)
             # missing_found += 1
     # print(missing_nums)
 
     missing_found = len(missing_nums)
-    # Output ranges
-    ranges = compute_ranges(missing_nums)
-    for i in ranges:
+    # Output missing number ranges
+    missing_ranges = compute_ranges(missing_nums)
+    for i in missing_ranges:
         print("Missing log line(s): {}".format(i))
 
     # Output Summary
@@ -310,6 +318,8 @@ if __name__ == '__main__':
             if args.output is None:
                 print("Must specify --output with --no-verify")
                 exit(1)
+        print("Starting to dump index...")
         dump = es.dump_index(args.index, output=args.output)
+        print("Done dumping index")
         if args.no_verify is False:
             verify_els_messages(dump, args.max)
